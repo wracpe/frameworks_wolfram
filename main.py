2b9f6aa2049612d6e504bd42d3b707b3d10fd4dc
m0squit <platyna@bk.ru>
2021-05-26 12:49:15 +0500

    fix main calculation settings

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ config_vyngayakhinskoe = ConfigField(
     target='ql_m3_fact',
     estimator_name_field='xgb',
     estimator_name_well='ela',
-    is_deep_grid_search=True,
-    window_sizes=[7],
-    quantiles=[0.9],
+    is_deep_grid_search=False,
+    window_sizes=[5, 10, 15, 20],
+    quantiles=[0.7, 0.9],
     well_names_ois=[
         '2860077600',
     ],
